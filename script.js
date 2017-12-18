ba6364fb059797ca35e9375d84c161662ddd8230
TR246 <32668942+TR246@users.noreply.github.com>
2017-12-18 19:14:46 +0900

    propバインドどうしよ

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 (() => {
     //バインド
+    const bindedSymbol = Symbol();
+    const propBindedSymbol = Symbol();
+    const watchers = {};
     const bindElement = (el, obj, key, onchange) => {
+        if(el[bindedSymbol]){ return; }
+        el[bindedSymbol] = true;
         switch(el.tagName.toLowerCase()){
             case "input":
                 if(["text", "number"].includes(el.type)){
-                    el.value = bindingData[key];
+                    el.value = obj[key];
                     el.addEventListener("input", () => {
                         const value = el.value;
-                        bindingData[key] = value;
-                        onchange(key, value);
+                        obj[key] = value;
+                        if(onchange) onchange(key, value);
+                        if(watchers[key]) watchers[key].forEach(fn => fn(value));
                     });
                 }
                 break;
 
             case "select":
-                el.selectedIndex = bindingData[key];
+                el.selectedIndex = obj[key];
                 el.addEventListener("change", () => {
                     const value = el.selectedIndex;
-                    bindingData[key] = value;
-                    onchange(key, value);
+                    obj[key] = value;
+                    if(onchange) onchange(key, value);
+                    if(watchers[key]) watchers[key].forEach(fn => fn(value));
                 });
                 break;
         }
@@ -26,13 +33,19 @@
     const bindingData = {
         width: 2250,
         height: 600,
-        signType: 0
+        signType: 0,
+        signData: {
+            boardType: 2,
+            boardLighting: true,
+            staNameKanji: "東京",
+            staNameEnglish: "Tōkyō",
+            staNameKana: "とうきょう",
+            staNameChinese: "东京",
+            staNameKorean: "도쿄"
+        }
     };
-    Array.from(document.querySelectorAll("[data-bind]"))
-        .forEach(el => bindElement(el, bindingData, el.getAttribute("data-bind"), (key, value) => {
-            document.getElementById("test").textContent = JSON.stringify(bindingData) + "\n変更されたキー: " + key + ", 変更された値: " + value;
-        }));
-
-    //generatordataから初期化
-    const generatorData = JSON.parse(document.getElementById("generatorData").textContent);
+    Array.from(document.getElementById("general").querySelectorAll("[data-bind]"))
+        .forEach(el => bindElement(el, bindingData, el.getAttribute("data-bind")));
+    Array.from(document.getElementById("generator").querySelectorAll("[data-bind]"))
+        .forEach(el => bindElement(el, bindingData.signData, el.getAttribute("data-bind")));
 })();
